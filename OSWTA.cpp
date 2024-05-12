10ec4563c32f8f40b85aaf1076384a00d0a9acbc
GAkl224 <156771707+GAkl224@users.noreply.github.com>
2024-05-12 05:41:59 -0500

    (Three Credits) String Input Types

--- OSWTA.cpp
+++ OSWTA.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <limits>
+#include <string>
 
 using namespace std;
 
-
-int prompt(int lowBound, int upBound, string promt, string error) {
+//
+int prompt(int lowBound, int upBound, string prompt, string error) {
     int value;
     bool validInput = false;
 
@@ -15,8 +16,29 @@ int prompt(int lowBound, int upBound, string promt, string error) {
 
         // Check if input is valid
         if (cin.fail() || value < lowBound || value > upBound) {
-            cin.clear(); 
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); /
+            cin.clear(); // Clear error flags
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
+            cout << error << endl;
+        } else {
+            validInput = true;
+        }
+    } while (!validInput);
+
+    return value;
+}
+
+// prompt the user for a string input
+string prompt( string prompt,  string error) {
+    string value;
+    bool validInput = false;
+
+    // Repeat until valid input is received
+    do {
+        cout << prompt << endl;
+        getline(cin, value);
+
+        // Check if input is valid
+        if (value.empty()) {
             cout << error << endl;
         } else {
             validInput = true;
@@ -27,9 +49,13 @@ int prompt(int lowBound, int upBound, string promt, string error) {
 }
 
 int main() {
+    //integer input
+    int int_value = prompt(0, 100, "Please enter an integer value", "Your integer value is invalid");
+    cout << "The integer value chosen by the user is " << int_value << endl;
     
-    int value = prompt(0, 100, "Please enter a value", "Your value is invalid");
-    cout << "The value chosen by the user is " << value << endl;
-    
+    // string input
+    string str_value = prompt("Please enter a string value", "Your string value is invalid");
+    cout << "The string value chosen by the user is \"" << str_value << "\"" << endl;
+
     return 0;
 }
